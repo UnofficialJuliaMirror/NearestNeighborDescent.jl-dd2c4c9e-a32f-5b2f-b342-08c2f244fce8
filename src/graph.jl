887594ac52b250629bec17b82246b566eaede492
Dillon Daudert <dillongdaudert@gmail.com>
2019-07-25 18:01:26 -0400

    Partial implement types

--- src/graph.jl
+++ src/graph.jl
@@ -1,20 +1,47 @@
 # Implement the AbstractGraph interface from LightGraphs.jl
 
-import LightGraphs
+
+import LightGraphs: AbstractGraph, AbstractEdge
+import Distances: PreMetric
+import DataStructures: AbstractHeap
+
+abstract type AbstractDescentEdge{T <: Integer, U <: Real} <: AbstractEdge{T} end
+
+struct DescentEdge{T, U} <: AbstractDescentEdge{T, U}
+    srcidx::T
+    dstidx::T
+    dist::U
+    flag::Bool
+end
+
+abstract type AbstractDescentGraph <: AbstractGraph end
 
 """
 A directed graph where each node has edges to its (approximate) k-nearest
 neighbors.
 """
-struct DescentGraph <: AbstractGraph end
+struct DescentGraph{VT,
+                    DT <: AbstractVector{VT},
+                    M <: PreMetric,
+                    HT,
+                    H <: AbstractVector{<:AbstractHeap{HT}}} <: AbstractDescentGraph
+    data::DT
+    metric::M
+    _knn_heaps::H
+end
 
 
 """
 Return an iterator (or collection of) the edges of a graph.
 """
+# Presently, edges are stored as knn_heaps[i][j].idx. The source is
+# the index into the heaps, the dest is the `idx` field on the
+# element tuples.
 function edges(g::DescentGraph) end
 
-function Base.eltype(g::DescentGraph) end
+# The *technical* element types of the descent graph are
+# the points (vectors) in the dataset.
+Base.eltype(g::DescentGraph{VT}) where VT = VT
 
 function edgetype(g::DescentGraph) end
 
@@ -23,11 +50,19 @@ function has_edge(g::DescentGraph, s, d) end
 # (optional)
 function has_edge(g::DescentGraph, e) end
 
+# if the element type are points / vectors, this might be
+# tricky. strings would work fine... but it might depend on
+# if the same vector / data point can exist as separate
+# vertices, or not...
 function has_vertex(g::DescentGraph, v) end
 
 """
 Return a list of all neighbors connected to vertex `v` by an incoming edge.
 """
+# with the current knn heaps structure, inneighbors will be
+# much less efficient than outneighbors. it will require
+# traversing all the knn heaps to find outneighbors with
+# `v` as the destination
 function inneighbors(g::DescentGraph, v) end
 
 function outneighbors(g::DescentGraph, v) end
